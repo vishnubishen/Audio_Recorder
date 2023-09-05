54ce0634bf1edded63f53403a3de7e8162026755
Vishnu_Balasubramaniyan <vishnu.balasubramaniyan@axeno.co>
2023-09-05 13:59:44 +0530

    Audio recorder initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import RecordRTC from "recordrtc";
 
-function App() {
+function AudioRecorder() {
+  const [stream, setStream] = useState(null);
+  const [recorder, setRecorder] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioFiles, setAudioFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  useEffect(() => {
+    // Check if the user has granted microphone permission
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        setStream(stream);
+        setRecorder(RecordRTC(stream, { type: "audio" }));
+      })
+      .catch((error) => {
+        console.error("Error accessing microphone:", error);
+      }, []);
+
+    return () => {
+      // Release the microphone when the component unmounts
+      if (stream) {
+        stream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  const startRecording = () => {
+    if (recorder) {
+      recorder.startRecording();
+      setIsRecording(true);
+    }
+  };
+
+  const stopRecording = () => {
+    if (recorder) {
+      recorder.stopRecording(() => {
+        const blob = recorder.getBlob();
+        const audioURL = URL.createObjectURL(blob);
+        const newAudioFile = { audioURL, name: "sample" };
+        setAudioFiles([...audioFiles, newAudioFile]);
+        setIsRecording(false);
+      });
+    }
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    debugger;
+  };
+
+  const uploadAudio = () => {
+    if (selectedFile) {
+      const audioURL = URL.createObjectURL(selectedFile);
+      const newAudioFile = { audioURL, name: selectedFile.name };
+      setAudioFiles([...audioFiles, newAudioFile]);
+      setSelectedFile(null);
+    }
+  };
+
+  const playAudio = (audioURL) => {
+    const audioElement = new Audio(audioURL);
+    audioElement.play();
+  };
+
+  const downloadAudio = (audioURL, audioName) => {
+    const a = document.createElement("a");
+    a.href = audioURL;
+    a.download = audioName;
+    a.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Audio Recorder and Uploader</h2>
+      {isRecording ? (
+        <>
+          <button onClick={stopRecording}>Stop Recording</button>
+        </>
+      ) : (
+        <>
+          <button onClick={startRecording}>Start Recording</button>
+          {!stream && <span>Allow microphoen access</span>}
+          <br />
+          <br />
+
+          <input
+            type="file"
+            accept="audio/*"
+            onChange={handleFileInputChange}
+          />
+          <button onClick={uploadAudio} disabled={!selectedFile}>
+            Upload Audio
+          </button>
+        </>
+      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Serial Number</th>
+            <th>Audio Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {audioFiles.map((file, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <input
+                  type="text"
+                  value={file.name}
+                  onChange={(e) => {
+                    const updatedAudioFiles = [...audioFiles];
+                    updatedAudioFiles[index].name = e.target.value;
+                    setAudioFiles(updatedAudioFiles);
+                  }}
+                />
+              </td>
+              <td>
+                <button onClick={() => playAudio(file.audioURL)}>Play</button>
+                <button onClick={() => downloadAudio(file.audioURL, file.name)}>
+                  Download
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default AudioRecorder;
